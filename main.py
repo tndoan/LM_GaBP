50500d9022741bcdd1ede0cce2a883d51d4d1293
tndoan <doan.thanh.nam.1988@gmail.com>
2014-04-20 21:20:15 +0800

    fix the bug of reading file. But still has bug when size of matrix is greater than 3

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ def construct_graph(A, b):
     # add prior of each node
     for i in range(size):
         # prior is equivalent to phi function in paper
-        result.add_node(i, prior_mean=b[i]/A[i][i], prior_precision=A[i][i],\
+        result.add_node(i, prior_mean=float(b[i])/A[i][i], prior_precision=float(A[i][i]),\
                 mean=0.0, prec=0.0) 
     
     # add value between 2 nodes
     for i in range(size):
         for j in range(size):
-            if A[i][j] != 0: # add if the value is not zero
+            if A[i][j] != 0 and i != j: # add if the value is not zero
                 result.add_edge(i, j, mean=0.0, precision=0.0)
     return result
 
@@ -40,12 +40,54 @@ def check_continue(old_graph, graph, threshold):
     old graph and new graph is less than the threshold"""
     nodes = graph.nodes()
     mean_difference = 0.0
+    #for i in nodes:
+    #    difference = abs(old_graph.node[i]['mean'] - graph.node[i]['mean'])
+    #    mean_difference += difference
     for i in nodes:
-        difference = abs(old_graph[i]['mean'] - graph[i]['mean'])
-        mean_difference += difference
+        for j in nodes:
+            if graph[i].get(j) != None:
+                mean_difference += abs(old_graph[i][j]['mean'] - graph[i][j]['mean'])
+                mean_difference += abs(old_graph[i][j]['precision'] - graph[i][j]['precision'])
     if mean_difference < threshold:
         return False # the total sum is less than threshold
     return True
+
+def iterate_until_convergence(graph, threshold, A):
+    # solve the symmetric case
+    isContinue = True
+    nodes = graph.nodes() # set of nodes in graph
+    count = 0
+    while isContinue and count < 5:
+        old_graph = graph.copy()
+        
+        # update the marginal distribution of each node
+        for i in nodes:
+            node_info = graph.node[i]
+            graph.node[i]['prec'] = graph.node[i]['prior_precision']
+            temp = 0.0
+            for j in nodes:
+                neighbor = graph[i].get(j) 
+                if neighbor != None: # it is actual neighbor
+                    graph.node[i]['prec'] += graph[j][i]['precision']
+                    temp += graph[j][i]['mean'] * graph[j][i]['precision']
+            graph.node[i]['mean'] = (1.0/graph.node[i]['prec']) \
+                    * (graph.node[i]['prior_mean'] * graph.node[i]['prior_precision'] + temp)
+
+        # update the distribution of message
+        for i in nodes:
+            for j in nodes:
+                node_info = graph[i].get(j)
+                if node_info != None:
+                    graph[i][j]['precision'] = -float(A[i][j]) * float(A[i][j]) /(graph.node[i]['prec'] - graph[j][i]['precision'])
+                    graph[i][j]['mean'] = (graph.node[i]['prec'] * graph.node[i]['mean'] \
+                            - graph[j][i]['precision'] * graph[j][i]['mean'])/float(A[i][j])
+        isContinue = check_continue(old_graph, graph, threshold)
+        #print graph.node
+        #print graph.edge
+        count += 1
+        print count # print number of iteration
+        print "======"
+    return graph
 
 if __name__ == "__main__":
     filename1 = sys.argv[1]
@@ -57,42 +99,13 @@ if __name__ == "__main__":
     # check symmetric of matrix A
     if not is_satisfied(A, b):
         print "The matrix and vector from input are not valid"
-        return
+        sys.exit(0)
 
     # initialization
-    mean = np.zeros(A.shape[0])
-    precision = np.ones(A.shape[0])
     graph = construct_graph(A, b)
-    nodes = graph.nodes() # set of nodes in graph
-    
-    # solve the symmetric case
-    isContinue = True
-    while isContinue:
-        old_graph = graph.copy()
-        
-        # update the marginal distribution of each node
-        for i in nodes:
-            node_info = graph.node[i]
-            graph[i]['prec'] = graph[i]['prior_precision']
-            temp = 0.0
-            for j in nodes:
-                neighbor = graph[i].get(j) 
-                if neighbor != None: # it is actual neighbor
-                    graph[i]['prec'] += graph[j][i]['precision']
-                    temp += graph[j][i]['mean'] * graph[j][i]['precision']
-            graph[i]['mean'] = (1.0/graph[i]['prec']) \
-                    * (graph[i]['prior_mean'] * graph[i]['prior_precision'] + temp)
-
-        # update the distribution of message
-        for i in nodes:
-            for j in nodes:
-                node_info = graph[i].get(j)
-                if node_info != None:
-                    graph[i][j]['precision'] = -A[i][j] * A[i][j] /(graph[i]['prec'] - graph[j][i]['precision'])
-                    graph[i][j]['mean'] = (graph[i]['prec'] * graph[i]['mean'] \
-                            - graph[j][i]['precision'] * graph[j][i]['mean'])/A[i][j]
-        isContinue = check_continue(old_graph, graph, threshold)
+   
+    result_graph = iterate_until_convergence(graph, threshold, A)
 
     # print result 
-    for i in nodes:
-        print graph[i]['mean']
+    #for i in nodes:
+    #    print result_graph[i]['mean']
