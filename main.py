70d1e401f75d243de8f6ba6208a7947cecf1d043
tndoan <doan.thanh.nam.1988@gmail.com>
2014-04-20 22:01:31 +0800

    the bug in previous commit is because of loop in graphical model. Code has been cleaned and added some comments

--- main.py
+++ main.py
@@ -21,13 +21,15 @@ def construct_graph(A, b):
     result = nx.DiGraph()
     size = A.shape[0]
     
-    # add prior of each node
+    # initialize prior and marginal distribution for each node
+    # mean and prec is the parameters of marginal distribution
     for i in range(size):
         # prior is equivalent to phi function in paper
         result.add_node(i, prior_mean=float(b[i])/A[i][i], prior_precision=float(A[i][i]),\
                 mean=0.0, prec=0.0) 
     
-    # add value between 2 nodes
+    # initialize the distribution of message between 2 nodes
+    # g[i][j] = message from i to j. It is Gaussian distribution
     for i in range(size):
         for j in range(size):
             if A[i][j] != 0 and i != j: # add if the value is not zero
@@ -39,25 +41,22 @@ def check_continue(old_graph, graph, threshold):
     But this terminates if the sum of mean difference of nodes in
     old graph and new graph is less than the threshold"""
     nodes = graph.nodes()
-    mean_difference = 0.0
-    #for i in nodes:
-    #    difference = abs(old_graph.node[i]['mean'] - graph.node[i]['mean'])
-    #    mean_difference += difference
+    total_difference = 0.0
     for i in nodes:
-        for j in nodes:
-            if graph[i].get(j) != None:
-                mean_difference += abs(old_graph[i][j]['mean'] - graph[i][j]['mean'])
-                mean_difference += abs(old_graph[i][j]['precision'] - graph[i][j]['precision'])
-    if mean_difference < threshold:
+        difference = abs(old_graph.node[i]['mean'] - graph.node[i]['mean'])
+        total_difference += difference
+    if total_difference < threshold:
         return False # the total sum is less than threshold
     return True
 
 def iterate_until_convergence(graph, threshold, A):
-    # solve the symmetric case
+    """iteration through graph to find the solution"""
     isContinue = True
     nodes = graph.nodes() # set of nodes in graph
-    count = 0
-    while isContinue and count < 5:
+    count = 0 # count the number of iteration
+
+    # run untial 1000 iterations or convergence
+    while isContinue and count < 1000:
         old_graph = graph.copy()
         
         # update the marginal distribution of each node
@@ -82,14 +81,11 @@ def iterate_until_convergence(graph, threshold, A):
                     graph[i][j]['mean'] = (graph.node[i]['prec'] * graph.node[i]['mean'] \
                             - graph[j][i]['precision'] * graph[j][i]['mean'])/float(A[i][j])
         isContinue = check_continue(old_graph, graph, threshold)
-        #print graph.node
-        #print graph.edge
         count += 1
-        print count # print number of iteration
-        print "======"
     return graph
 
 if __name__ == "__main__":
+    """solve the problem A x = b using probabilistic graphical model"""
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
     threshold = 0.01
@@ -101,11 +97,12 @@ if __name__ == "__main__":
         print "The matrix and vector from input are not valid"
         sys.exit(0)
 
-    # initialization
-    graph = construct_graph(A, b)
-   
+    graph = construct_graph(A, b) # initialization
+  
+    # iterate to solve
     result_graph = iterate_until_convergence(graph, threshold, A)
 
     # print result 
-    #for i in nodes:
-    #    print result_graph[i]['mean']
+    nodes = result_graph.nodes()
+    for i in nodes:
+        print result_graph.node[i]['mean']
